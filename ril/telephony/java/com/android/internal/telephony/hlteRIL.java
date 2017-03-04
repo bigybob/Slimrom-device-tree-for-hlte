8d832dfc7ce053846cfadac760f626bc85b3502c
sparx <alliancesparx@gmail.com>
2017-03-04 22:08:12 +0000

    Fix compilation errors

--- ril/telephony/java/com/android/internal/telephony/hlteRIL.java
+++ ril/telephony/java/com/android/internal/telephony/hlteRIL.java
@@ -259,8 +259,7 @@ public class hlteRIL extends RIL implements CommandsInterface {
                                 dc.uusInfo.getUserData().length));
                 riljLogv("Incoming UUS : data (string)="
                         + new String(dc.uusInfo.getUserData()));
-                riljLogv("Incoming UUS : data (hex): "
-                        + IccUtils.bytesToHexString(dc.uusInfo.getUserData()));
+  
             } else {
                 riljLogv("Incoming UUS : NOT present!");
             }
@@ -294,7 +293,7 @@ public class hlteRIL extends RIL implements CommandsInterface {
 
     @Override
     protected void
-    processUnsolicited (Parcel p) {
+    processUnsolicited (Parcel p, int type) {
         Object ret;
         int dataPosition = p.dataPosition(); // save off position within the Parcel
         int response = p.readInt();
@@ -341,7 +340,7 @@ public class hlteRIL extends RIL implements CommandsInterface {
                 p.setDataPosition(dataPosition);
 
                 // Forward responses that we are not overriding to the super class
-                super.processUnsolicited(p);
+                super.processUnsolicited(p, type);
                 return;
         }
 
@@ -362,7 +361,7 @@ public class hlteRIL extends RIL implements CommandsInterface {
 
     @Override
     protected RILRequest
-    processSolicited (Parcel p) {
+    processSolicited (Parcel p, int type) {
         int serial, error;
         boolean found = false;
         int dataPosition = p.dataPosition(); // save off position within the Parcel
@@ -396,7 +395,7 @@ public class hlteRIL extends RIL implements CommandsInterface {
             /* Nothing we care about, go up */
             p.setDataPosition(dataPosition);
             // Forward responses that we are not overriding to the super class
-            return super.processSolicited(p);
+            return super.processSolicited(p, type);
         }
         rr = findAndRemoveRequestFromList(serial);
         if (rr == null) {
